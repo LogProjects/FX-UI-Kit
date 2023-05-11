ac438d4bed1175593a1bbd097140c7b66405a9de
SinirliJapon <mkemalerylmz@gmail.com>
2023-05-11 16:32:24 +0200

    Chevron_icons edited

--- lib/components/calendar/calendar.dart
+++ lib/components/calendar/calendar.dart
@@ -28,74 +28,107 @@ class _CalendarState extends State<Calendar> {
             width: 0.5,
           ),
         ),
-        child: TableCalendar(
-          rowHeight: 38,
-          firstDay: DateTime.utc(2020, 01, 01),
-          lastDay: DateTime.utc(2030, 12, 31),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onHover: (event) {},
+          child: TableCalendar(
+            firstDay: DateTime.utc(2020, 01, 01),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            rowHeight: 38,
+            daysOfWeekHeight: 30,
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            onPageChanged: (focusedDay) {
               _focusedDay = focusedDay;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            _focusedDay = focusedDay;
-          },
-          headerStyle: const HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextStyle: TextStyle(fontSize: 14, height: 1.2),
-            headerPadding: EdgeInsets.symmetric(vertical: 4),
-          ),
-          daysOfWeekStyle: DaysOfWeekStyle(
-            weekdayStyle: const TextStyle(
-                fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
-            weekendStyle: const TextStyle(
-                fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
-            dowTextFormatter: (date, locale) =>
-                DateFormat.E(locale).format(date).substring(0, 2).toUpperCase(),
-          ),
-          calendarStyle: const CalendarStyle(
-            todayDecoration: BoxDecoration(
-              color: ThemeColor.Grey,
-              shape: BoxShape.rectangle,
-            ),
-            selectedDecoration: BoxDecoration(
-              color: ThemeColor.Primary,
-              shape: BoxShape.rectangle,
-            ),
-            defaultTextStyle: TextStyle(fontSize: 14, height: 1.2),
-            weekendTextStyle: TextStyle(fontSize: 14, height: 1.2),
-            outsideTextStyle: TextStyle(
-                fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
-            todayTextStyle:
-                TextStyle(fontSize: 14, color: ThemeColor.Black, height: 1.2),
-            selectedTextStyle:
-                TextStyle(fontSize: 14, color: ThemeColor.White, height: 1.2),
-            disabledTextStyle:
-                TextStyle(fontSize: 14, color: ThemeColor.Black, height: 1.2),
-          ),
-          calendarBuilders: CalendarBuilders(
-              weekNumberBuilder: (BuildContext context, int weekNumber) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: MouseRegion(
-                child: Text(
-                  weekNumber.toString(),
-                  style: const TextStyle(fontSize: 14, height: 1.2),
+            },
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: const TextStyle(
+                  fontSize: 14, height: 1.2, fontWeight: FontWeight.bold),
+              leftChevronIcon: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: ThemeColor.Grey,
+                    width: 0.5,
+                  ),
+                ),
+                child: const Icon(
+                  Icons.chevron_left,
+                  color: ThemeColor.DarkGrey,
                 ),
               ),
-            );
-          }),
-          daysOfWeekHeight: 28,
-          availableCalendarFormats: const {
-            CalendarFormat.month: 'Month',
-          },
+              rightChevronIcon: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: ThemeColor.Grey,
+                    width: 0.5,
+                  ),
+                ),
+                child:
+                    const Icon(Icons.chevron_right, color: ThemeColor.DarkGrey),
+              ),
+              leftChevronMargin: const EdgeInsets.all(4),
+              rightChevronMargin: const EdgeInsets.all(4),
+              leftChevronPadding: const EdgeInsets.all(4),
+              rightChevronPadding: const EdgeInsets.all(4),
+            ),
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: const TextStyle(
+                  fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
+              weekendStyle: const TextStyle(
+                  fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
+              dowTextFormatter: (date, locale) => DateFormat.E(locale)
+                  .format(date)
+                  .substring(0, 2)
+                  .toUpperCase(),
+            ),
+            calendarStyle: const CalendarStyle(
+              todayDecoration: BoxDecoration(
+                color: ThemeColor.Grey,
+                shape: BoxShape.rectangle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: ThemeColor.Primary,
+                shape: BoxShape.rectangle,
+              ),
+              defaultTextStyle: TextStyle(fontSize: 14, height: 1.5),
+              weekendTextStyle: TextStyle(fontSize: 14, height: 1.2),
+              outsideTextStyle: TextStyle(
+                  fontSize: 14, height: 1.2, color: ThemeColor.DarkGrey),
+              todayTextStyle:
+                  TextStyle(fontSize: 14, color: ThemeColor.Black, height: 1.2),
+              selectedTextStyle:
+                  TextStyle(fontSize: 14, color: ThemeColor.White, height: 1.2),
+              disabledTextStyle:
+                  TextStyle(fontSize: 14, color: ThemeColor.Black, height: 1.2),
+            ),
+            calendarBuilders: CalendarBuilders(
+              weekNumberBuilder: (BuildContext context, int weekNumber) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    weekNumber.toString(),
+                    style: const TextStyle(fontSize: 14, height: 1.2),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     );
