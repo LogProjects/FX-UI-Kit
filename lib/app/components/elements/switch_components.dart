9a4c6d8c6ace6648a207814aec42ffc720ec2d0a
SinirliJapon <mkemalerylmz@gmail.com>
2023-05-23 12:45:28 +0200

    [Component] - SwitchToggle added

---
+++ lib/app/components/elements/switch_components.dart
@@ -0,0 +1,22 @@
+import 'package:f_kit_x/app/components/component_display.dart';
+import 'package:f_kit_x/app/components/component_list.dart';
+import 'package:f_kit_x/components/switch/switch.dart';
+import 'package:flutter/material.dart';
+
+class SwitchComponents extends StatelessWidget {
+  const SwitchComponents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ComponentList(
+      components: [
+        ComponentDisplay(
+          component: SwitchToggle(
+            label: 'Airplane Mode',
+            onToggle: (_) {},
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/components/switch/switch.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+
+class SwitchToggle extends StatefulWidget {
+  final bool isToggled;
+  final String label;
+  final void Function(bool)? onToggle;
+
+  const SwitchToggle({
+    Key? key,
+    this.isToggled = false,
+    required this.label,
+    this.onToggle,
+  }) : super(key: key);
+
+  @override
+  State<SwitchToggle> createState() => _SwitchToggleState();
+}
+
+class _SwitchToggleState extends State<SwitchToggle> {
+  late bool isToggled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isToggled = widget.isToggled;
+  }
+
+  void _onValueChanged(bool newValue) {
+    setState(() => isToggled = newValue);
+    if (widget.onToggle != null) {
+      widget.onToggle!(newValue);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlutterSwitch(
+          width: 44.0,
+          height: 24.0,
+          toggleSize: 20.0,
+          borderRadius: 30.0,
+          padding: 2.0,
+          activeColor: Colors.black,
+          inactiveColor: const Color(0xFFE2E8F0),
+          value: isToggled,
+          onToggle:
+              widget.onToggle != null ? _onValueChanged : (bool newValue) {},
+        ),
+        const SizedBox(width: 8),
+        Text(
+          widget.label,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+        )
+      ],
+    );
+  }
+}
