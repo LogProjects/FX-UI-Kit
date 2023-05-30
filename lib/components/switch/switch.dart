c622b7725d567052970e671933e49a661deb5e9e
Mustafa Kemal ERYILMAZ <74076865+SinirliJapon@users.noreply.github.com>
2023-05-30 22:50:59 +0200

    [Component] - Switch (#12)
    * [Component] - SwitchToggle added
    
    * [Color] - InactiveColor added
    
    * [Color] - activeColor changed
    
    * [Component] - InactiveColor edited
    
    * [Component] - LabelText added
    
    * [Component] - LabelText removed
    
    * Dart version update
    
    * [Component] Required callback function
    
    ---------
    
    Co-authored-by: erol <logaritma26@protonmail.com>

---
+++ lib/components/switch/switch.dart
@@ -0,0 +1,52 @@
+import 'package:f_kit_x/resource/theme_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+
+class SwitchToggle extends StatefulWidget {
+  final bool isToggled;
+  final void Function(bool) onToggle;
+
+  const SwitchToggle({
+    Key? key,
+    this.isToggled = false,
+    required this.onToggle,
+  }) : super(key: key);
+
+  @override
+  State<SwitchToggle> createState() => _SwitchToggleState();
+}
+
+class _SwitchToggleState extends State<SwitchToggle> {
+  late bool isToggled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isToggled = widget.isToggled;
+  }
+
+  void _onValueChanged(bool newValue) {
+    setState(() => isToggled = newValue);
+    widget.onToggle(newValue);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlutterSwitch(
+          width: 44.0,
+          height: 24.0,
+          toggleSize: 20.0,
+          borderRadius: 30.0,
+          padding: 2.0,
+          activeColor: ThemeColor.Primary,
+          inactiveColor: ThemeColor.InactiveColor,
+          value: isToggled,
+          onToggle: _onValueChanged,
+        ),
+      ],
+    );
+  }
+}
